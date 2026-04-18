69aa9f5632fac0e8ec1961b8584a834ea7f18379
Maksym Dazidov <maksymdazidov@gmail.com>
2026-04-18 14:51:15 +0200

    3

--- app/main.py
+++ app/main.py
@@ -22,13 +22,15 @@ def sha256_hash_str(to_hash: str) -> str:
 
 def brute_force_password() -> None:
     count = 0
+    TARGET_HASHES = set(PASSWORDS_TO_BRUTE_FORCE)
+    target_count = len(TARGET_HASHES)
     for i in range(100000000):
         pwd_str = f"{i:08d}"
         password = sha256_hash_str(pwd_str)
-        if password in set(PASSWORDS_TO_BRUTE_FORCE):
+        if password in set(TARGET_HASHES):
             count += 1
             print(f"found password №{count}:", pwd_str)
-        if count == len(PASSWORDS_TO_BRUTE_FORCE):
+        if count == target_count:
             break
 
 
